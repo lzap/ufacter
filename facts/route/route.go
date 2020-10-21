c3b66e15fa018596fb1f4fa56e1bd03b3bc61a15
Lukas Zapletal <lzap+git@redhat.com>
2020-10-21 16:01:23 +0200

    Fix default interface credentials

--- facts/route/route.go
+++ facts/route/route.go
@@ -30,7 +30,7 @@ func ReportFacts(facts chan<- ufacter.Fact, volatile bool, extended bool) {
 			c.LogError(facts, err, "link", "link by index")
 		}
 	} else {
-		c.LogError(facts, err, "link", "IPv4 route")
+		c.LogError(facts, err, "link", "IPv4 netlink default route")
 	}
 
 	// primary IPv6 interface
@@ -45,54 +45,53 @@ func ReportFacts(facts chan<- ufacter.Fact, volatile bool, extended bool) {
 			c.LogError(facts, err, "link", "link by index")
 		}
 	} else {
-		c.LogError(facts, err, "link", "IPv6 route")
+		c.LogError(facts, err, "link", "IPv6 netlink default route")
 	}
 
 	// networking.(mac,mtu,ip,ip6,netmask,netmask6,network,network6) + (mac6,mtu6) extended
 	netIfaces, err := s.Interfaces()
-	if err != nil {
-		c.LogError(facts, err, "route", "interfaces")
-		return
-	}
+	if err == nil {
+		for _, v := range netIfaces {
+			if primaryIPv4MAC != "" && v.HardwareAddr == primaryIPv4MAC {
+				// IPv4
+				facts <- ufacter.NewStableFact(v.HardwareAddr, "networking", "mac")
+				facts <- ufacter.NewStableFact(v.MTU, "networking", "mtu")
 
-	for _, v := range netIfaces {
-		if v.HardwareAddr == primaryIPv4MAC && len(primaryIPv6MAC) > 0 {
-			// IPv4
-			facts <- ufacter.NewStableFact(v.HardwareAddr, "networking", "mac")
-			facts <- ufacter.NewStableFact(v.MTU, "networking", "mtu")
+				for _, ipAddr := range v.Addrs {
+					parsedIP, parsedNet, err := net.ParseCIDR(ipAddr.Addr)
+					if err != nil {
+						c.LogError(facts, err, "route", "parse CIDR")
+						continue
+					}
+					if ip4 := parsedIP.To4(); ip4 != nil {
+						facts <- ufacter.NewStableFactEx(ipAddr.Addr, "networking", "cidr")
+						facts <- ufacter.NewStableFact(parsedIP.String(), "networking", "ip")
+						facts <- ufacter.NewStableFact(parsedNet.IP.String(), "networking", "network")
+						facts <- ufacter.NewStableFact(c.IPMaskToString4(parsedNet.Mask), "networking", "netmask")
+					}
+				}
+			} else if primaryIPv6MAC != "" && v.HardwareAddr == primaryIPv6MAC {
+				// IPv6
+				facts <- ufacter.NewStableFactEx(v.HardwareAddr, "networking", "mac6")
+				facts <- ufacter.NewStableFactEx(v.MTU, "networking", "mtu6")
 
-			for _, ipAddr := range v.Addrs {
-				parsedIP, parsedNet, err := net.ParseCIDR(ipAddr.Addr)
-				if err != nil {
-					c.LogError(facts, err, "route", "parse CIDR")
-					continue
-				}
-				if ip4 := parsedIP.To4(); ip4 != nil {
-					facts <- ufacter.NewStableFactEx(ipAddr.Addr, "networking", "cidr")
-					facts <- ufacter.NewStableFact(parsedIP.String(), "networking", "ip")
-					facts <- ufacter.NewStableFact(parsedNet.IP.String(), "networking", "network")
-					facts <- ufacter.NewStableFact(c.IPMaskToString4(parsedNet.Mask), "networking", "netmask")
-				}
-			}
-		} else if v.HardwareAddr == primaryIPv6MAC && len(primaryIPv6MAC) > 0 {
-			// IPv6
-			facts <- ufacter.NewStableFactEx(v.HardwareAddr, "networking", "mac6")
-			facts <- ufacter.NewStableFactEx(v.MTU, "networking", "mtu6")
-
-			for _, ipAddr := range v.Addrs {
-				parsedIP, parsedNet, err := net.ParseCIDR(ipAddr.Addr)
-				if err != nil {
-					c.LogError(facts, err, "route", "parse CIDR")
-					continue
-				}
-				if ip4 := parsedIP.To4(); ip4 == nil {
-					facts <- ufacter.NewStableFactEx(ipAddr.Addr, "networking", "cidr6")
-					facts <- ufacter.NewStableFact(parsedIP.String(), "networking", "ip6")
-					facts <- ufacter.NewStableFact(parsedNet.IP.String(), "networking", "network6")
-					facts <- ufacter.NewStableFact(c.IPMaskToString4(parsedNet.Mask), "networking", "netmask6")
+				for _, ipAddr := range v.Addrs {
+					parsedIP, parsedNet, err := net.ParseCIDR(ipAddr.Addr)
+					if err != nil {
+						c.LogError(facts, err, "route", "parse CIDR")
+						continue
+					}
+					if ip4 := parsedIP.To4(); ip4 == nil {
+						facts <- ufacter.NewStableFactEx(ipAddr.Addr, "networking", "cidr6")
+						facts <- ufacter.NewStableFact(parsedIP.String(), "networking", "ip6")
+						facts <- ufacter.NewStableFact(parsedNet.IP.String(), "networking", "network6")
+						facts <- ufacter.NewStableFact(c.IPMaskToString4(parsedNet.Mask), "networking", "netmask6")
+					}
 				}
 			}
 		}
+	} else {
+		c.LogError(facts, err, "route", "interfaces")
 	}
 
 	ufacter.SendVolatileFactEx(facts, time.Since(start), "ufacter", "stats", "route")
